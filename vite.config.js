57330ce8bbb4044c7accf751c11d74abd199798b
vldpvnnk <vldpvnnk@bk.ru>
2025-10-15 16:42:41 +0700

    feat(config): add github pages conf

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'src',
+  base: '/responsive-landing-page/',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
